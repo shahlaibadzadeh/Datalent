378eb4ea514ae9e327a1da8e4f8ccdb6ba90511b
Shahla <shahlaibadzadeh@gmail.com>
2020-11-23 01:16:47 +0400

    Testimonial

--- assets/js/test.js
+++ assets/js/test.js
@@ -11,24 +11,31 @@ $(document).ready(function(){
     {
     breakpoint: 1024,
     settings: {
-    slidesToShow: 3,
-    slidesToScroll: 3,
+    slidesToShow: 1,
+    slidesToScroll: 1,
     infinite: true,
-    dots: true
     }
     },
     {
+        breakpoint: 800,
+        settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            infinite: true,
+        }
+        },
+    {
     breakpoint: 600,
     settings: {
-    slidesToShow: 2,
-    slidesToScroll: 2
+    slidesToShow: 1,
+    slidesToScroll: 1,
     }
     },
     {
     breakpoint: 480,
     settings: {
     slidesToShow: 1,
-    slidesToScroll: 1
+    slidesToScroll: 1,
     }
     }
     
