e15fde413f9f987624459f37a6c1d1fb8921d5d0
shahlaibadzadeh <shahlaibadzadeh@gmail.com>
2020-12-14 02:10:20 +0400

    Responsiveness, Company Slider, Hover effects

--- assets/js/company-slider.js
+++ assets/js/company-slider.js
@@ -1,41 +1,87 @@
-$(document).ready(function(){
-    $('.items.company-items').slick({
-        infinite: false,
-        speed: 300,
-        slidesToShow: 6,
-        slidesToScroll: 6,
-    responsive: [
+
+
+$('.company-slider').slick({
+  dots: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 6,
+  slidesToScroll: 1,
+  responsive: [
     {
-    breakpoint: 1024,
-    settings: {
-    slidesToShow: 4,
-    slidesToScroll: 2,
-    infinite: false,
-    }
+      breakpoint: 1400,
+      settings: {
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: false
+      }
     },
     {
-        breakpoint: 800,
-        settings: {
-            slidesToShow: 3,
-            slidesToScroll: 3,
-            infinite: false,
-        }
-        },
-    {
-    breakpoint: 600,
-    settings: {
-    slidesToShow: 2,
-    slidesToScroll: 2
-    }
+      breakpoint: 1170,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: false
+      }
     },
     {
-    breakpoint: 480,
-    settings: {
-    slidesToShow: 1,
-    slidesToScroll: 1
+      breakpoint: 1150,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: false
+      }
+    },
+    {
+      breakpoint: 1100,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: false
+      }
+    },
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: false
+      }
+    },
+    {
+      breakpoint: 940,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: false
+      }
+    },
+    {
+      breakpoint: 730,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
     }
-    }
-    
-    ]
-    });
-    });
+  ]
+});
