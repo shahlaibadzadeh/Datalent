68bf888804e041845a65d7899873ff100b993e33
shahlaibadzadeh <shahlaibadzadeh@gmail.com>
2020-12-21 18:34:39 +0400

    Changed

--- assets/js/index.js
+++ assets/js/index.js
@@ -70,6 +70,8 @@ $('.first-order >h6').hover(function(){
 
 /*********************************************Fullpage.js*********************************** */
 
+
+
 function topFunction(){
   fullpage_api.moveTo(1);
 }
