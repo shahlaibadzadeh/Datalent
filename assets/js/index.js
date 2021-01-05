321897a69d8a199e400b912b5453e901741560d2
shahlaibadzadeh <shahlaibadzadeh@gmail.com>
2021-01-05 18:56:11 +0400

    Social media, arrow icons

--- assets/js/index.js
+++ assets/js/index.js
@@ -246,8 +246,8 @@ var fourthText = $('.fourth-text');
           var contactContainers = $('.contactScheduleAnimation');
 
 
-          var bottom_of_object = $(this).offset().top + $(this).outerHeight();
-          var bottom_of_window = $(window).scrollTop() + $(window).height()+200;
+          var bottom_of_object = $(this).offset().top+500;
+          var bottom_of_window = $(window).scrollTop() + $(window).height();
           
           if( bottom_of_window > bottom_of_object ){
               
